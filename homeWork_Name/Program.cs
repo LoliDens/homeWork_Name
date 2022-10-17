e66f2390874540705d4e529c5930df7a3db65d08
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-18 00:02:01 +0500

    Добавьте файлы проекта.

---
+++ homeWork_Name/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            char userSymbol;
+            int stringLongh;
+
+            Console.Write("Введите имя: ");
+            userName= Console.ReadLine();
+            Console.Write("Введите символ: ");
+            userSymbol = Char.Parse(Console.ReadLine());
+            stringLongh = userName.Length;
+
+            for (int i = 0; i < stringLongh; i++) 
+            {
+                Console.Write($"{userSymbol} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"{userSymbol} {userName} {userSymbol}");
+
+            for (int i = 0; i < stringLongh; i++)
+            {
+                Console.Write($"{userSymbol} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
